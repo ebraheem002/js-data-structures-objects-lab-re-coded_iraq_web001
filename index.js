6bdac810d31b586944f5dcdf40e409e71fa0ae8b
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-06-10 11:54:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let driver = {name:'sam'}
+function updateDriverWithKeyAndValue(driver,key,value){
+  driver ={ ...driver}
+  driver[key] = value
+
+  return driver
+}
+
+updateDriverWithKeyAndValue(driver,'address', '11 Broadway')
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
+
+  driver[key] = value
+
+  return driver
+}
+
+destructivelyUpdateDriverWithKeyAndValue(driver,'address','11 Broadway')
+
+function destructivelyDeleteFromDriverByKey(driver,key){
+  delete driver.kye;
+}
+
+destructivelyDeleteFromDriverByKey(driver,'name')
